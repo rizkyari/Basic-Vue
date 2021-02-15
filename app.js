204f899b2cfc9711ec9253c839674c05464223c0
rizkyari <rizkyari4121@gmail.com>
2021-02-15 10:34:57 +0700

    using methods

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
     data(){
         return {
             courseGoal: 'Learning Vue',
-            vueLink: 'https://vuejs.org/'
+            vueLink: 'https://vuejs.org/',
+            goalA: 'Finishing Vue Course',
+            goalB: 'Mastering Vue'
         };
+    },
+    methods: {
+        outputGoal(){
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return this.goalA;
+            }else {
+                return this.goalB;
+            }
+        }
     }
 });
 
